e10f13675c8831114e8f889782ced34908d56680
Mai Campbell <jirallc@gmail.com>
2020-04-14 19:43:13 -0700

    change port to be dynamically

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,5 @@
 const app = require('../server/index')
 
-app.listen(8000, function() {
-    console.log('Example app listening on port 8000!')
+app.listen(process.env.PORT || 8000 function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
